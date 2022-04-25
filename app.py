5d09cfd05222d3bc72a1f7eb4ca00712d3716a6b
Mike <wattonlaw@gmail.com>
2022-04-25 15:59:38 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,91 @@
+"""Flask app for browsing through cupcakes and getting info about each"""
+
+from flask import Flask, request, jsonify, render_template
+from importlib_metadata import re
+
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "secret"
+
+connect_db(app)
+
+
+@app.route("/")
+def root():
+    """shows homepage"""
+
+    return render_template("index.html")
+
+@app.route("/api/cupcakes")
+def list_cupcakes():
+    """Return all cupcakes in system.
+
+    Returns JSON like:
+        {cupcakes: [{id, flavor, rating, size, image}, ...]}
+    """
+
+    cupcakes = [cupcake.to_dict() for cupcake in Cupcake.query.all()]
+    return jsonify(cupcakes=cupcakes)
+
+@app.route("/api/cupcakes", methods=["POST"])
+def create_cupcake():
+    """Add cupcake, and return data about new cupcake.
+
+    Returns JSON like:
+        {cupcake: [{id, flavor, rating, size, image}]}
+    """
+
+    data = request.json
+
+    cupcake = Cupcake(
+        flavor=data['flavor'],
+        rating=data['rating'],
+        size=data['size'],
+        image=data['image'] or None
+    )
+
+    db.session.add(cupcake)
+    db.session.commit()
+
+    return (jsonify(cupcake=cupcake.to_dict()), 201)
+    # ensures a 201 status code is returned from a successful POST request
+
+@app.route("/api/cupcakes/<int:cupcake_id>")
+def get_cupcake(cupcake_id):
+    "returns JSON data on a particular cupcake"
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    return jsonify(cupcake=cupcake.to_dict())
+
+@app.route("/api/cupcakes/<int:cupcake_id>", methods=["PATCH"])
+def update_cupcake(cupcake_id):
+    """updates cupcake using data from request, and returnns updated data"""
+
+    data = request.json
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+
+    cupcake.flavor = data['flavor']
+    cupcake.rating = data['rating']
+    cupcake.size = data['size']
+    cupcake.image = data['image']
+
+    db.session.add(cupcake)
+    db.session.commit()
+
+    return jsonify(cupcake=cupcake.to_dict())
+
+@app.route("/api/cupcakes/<int:cupcake_id>", methods=["DELETE"])
+def delete_cupcake(cupcake_id):
+    """removes a cupcake from the system and returns a confirmation message"""
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+
+    db.session.delete(cupcake)
+    db.session.commit()
+
+    return jsonify(message="Cupcake deleted")
---
+++ static/cupcakes.js
@@ -0,0 +1,71 @@
+const BASE_URL = "http://localhost:5000/api";
+
+
+/** given data about a cupcake, generate html */
+
+function generateCupcakeHTML(cupcake) {
+  return `
+    <div data-cupcake-id=${cupcake.id}>
+      <li>
+        ${cupcake.flavor} / ${cupcake.size} / ${cupcake.rating}
+        <button class="delete-button">DELETE</button>
+      </li>
+      <img class="Cupcake-img"
+            src="${cupcake.image}"
+            alt="(no image provided)">
+    </div>
+  `;
+}
+
+
+/** put initial cupcakes on page. */
+
+async function showInitialCupcakes() {
+  const response = await axios.get(`${BASE_URL}/cupcakes`);
+    // function in the function in that page returns json data (dictionary - flavor: cherry, rating: 3, etc) for all cupcakes
+  for (let cupcakeData of response.data.cupcakes) {
+    let newCupcake = $(generateCupcakeHTML(cupcakeData));
+    $("#cupcakes-list").append(newCupcake);
+    // uses cupcake html function to turn each cupcake in response to html in the cupcakes list
+  }
+}
+
+
+/** handle form for adding of new cupcakes */
+
+$("#new-cupcake-form").on("submit", async function (evt) {
+  evt.preventDefault();
+
+  let flavor = $("#form-flavor").val();
+  let rating = $("#form-rating").val();
+  let size = $("#form-size").val();
+  let image = $("#form-image").val();
+
+  const newCupcakeResponse = await axios.post(`${BASE_URL}/cupcakes`, {
+    flavor,
+    rating,
+    size,
+    image
+  });
+//   someone sends in the form via a post request, this saves the cupcake info in that request to a variable
+
+  let newCupcake = $(generateCupcakeHTML(newCupcakeResponse.data.cupcake));
+  $("#cupcakes-list").append(newCupcake);
+  $("#new-cupcake-form").trigger("reset");
+  // takes the variable created immediately above this block, feeds it into the first function in this file, saves all that to a variable and appends it to page and resets form
+});
+
+
+/** handle clicking delete: delete cupcake */
+
+$("#cupcakes-list").on("click", ".delete-button", async function (evt) {
+  evt.preventDefault();
+  let $cupcake = $(evt.target).closest("div");
+  let cupcakeId = $cupcake.attr("data-cupcake-id");
+
+  await axios.delete(`${BASE_URL}/cupcakes/${cupcakeId}`);
+  $cupcake.remove();
+});
+
+
+$(showInitialCupcakes);
